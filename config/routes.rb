3c5d8e691903355cde02ede8f63f7b7655acb652
Pham Van Huy <phamvanhuy14@gmail.com>
2017-10-10 15:37:42 +0700

    chaper 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-
+  get "password_resets/new"
+  get "password_resets/edit"
   get  "/signup", to: "users#new"
   root "static_pages#home"
   get  "/help", to: "static_pages#help"
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
+
+  resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
