e479504f71562bf9d5e71fed4b19461de3495e48
Pham Van Huy <phamvanhuy14@gmail.com>
2017-10-10 15:37:42 +0700

    chaper 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :show, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :load_user, except: [:index, :new, :create]
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def new
     @user = User.new
   end
 
-  def show
-    begin
-      @user = User.find(params[:id])
-    rescue
-      flash[:danger] = t "users.new.user_not_found"
-      redirect_to root_path
-    end
-  end
+  def show; end
 
   def create
     @user = User.new user_params
@@ -23,8 +25,52 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "users.user.deleted"
+    else
+      flash[:danger] = t "users.user.delete_fail"
+    end
+    redirect_to users_url
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "users.edit.updated_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "users.edit.please_login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by_id params[:id]
+    redirect_to root_url unless @user == current_user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by_id params[:id]
+    return @user if @user
+    flash[:danger] = t "users.new.user_not_found"
+    redirect_to root_path
   end
 end
